58ecc953c7233392711ccf2ca382f55b9f5d8ec7
andi <127604728+andi-TA@users.noreply.github.com>
2024-06-25 05:50:16 +0000

    culture vf

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import {paysage} from './assets/index.js'
+import Bonjours from './bonjours.jsx'
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,10 +12,7 @@ function App() {
   return (
     <>
       <div>
-          <img src={paysage} className="logo" alt="Vite logo" />
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+         <Bonjours/>
       </div>
     </>
   )
